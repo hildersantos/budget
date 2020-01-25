3d6c53979edddf0d484a6f376465fe9d308b78bd
Hilder Santos <hilder@hildersantos.com>
2020-01-25 04:39:28 -0300

    Rename Accounts functions
    As "Accounts" is a Secondary Context (ref:
    https://devonestes.herokuapp.com/a-proposal-for-context-rules)
    we changed the function names to prevent verbosity.

--- test/budget/accounts_test.exs
+++ test/budget/accounts_test.exs
@@ -6,61 +6,61 @@ defmodule Budget.AccountsTest do
   describe "accounts" do
     alias Budget.Accounts.Account
 
-    @valid_attrs %{initial_balance: 42, name: "some name"}
-    @update_attrs %{initial_balance: 43, name: "some updated name"}
+    @valid_attrs %{initial_balance: 42000, name: "some name"}
+    @update_attrs %{initial_balance: 43000}
     @invalid_attrs %{initial_balance: nil, name: nil}
 
     def account_fixture(attrs \\ %{}) do
       {:ok, account} =
         attrs
         |> Enum.into(@valid_attrs)
-        |> Accounts.create_account()
+        |> Accounts.create()
 
       account
     end
 
-    test "list_accounts/0 returns all accounts" do
+    test "list/0 returns all accounts" do
       account = account_fixture()
-      assert Accounts.list_accounts() == [account]
+      assert Accounts.list() == [account]
     end
 
-    test "get_account!/1 returns the account with given id" do
+    test "get!/1 returns the account with given id" do
       account = account_fixture()
-      assert Accounts.get_account!(account.id) == account
+      assert Accounts.get!(account.id) == account
     end
 
-    test "create_account/1 with valid data creates a account" do
-      assert {:ok, %Account{} = account} = Accounts.create_account(@valid_attrs)
-      assert account.initial_balance == 42
+    test "create/1 with valid data creates a account" do
+      assert {:ok, %Account{} = account} = Accounts.create(@valid_attrs)
+      assert account.initial_balance == 42000
       assert account.name == "some name"
     end
 
-    test "create_account/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.create_account(@invalid_attrs)
+    test "create/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create(@invalid_attrs)
     end
 
-    test "update_account/2 with valid data updates the account" do
+    test "update/2 with valid data updates the account" do
       account = account_fixture()
-      assert {:ok, %Account{} = account} = Accounts.update_account(account, @update_attrs)
-      assert account.initial_balance == 43
-      assert account.name == "some updated name"
+      assert {:ok, %Account{} = account} = Accounts.update(account, @update_attrs)
+      assert account.initial_balance == 43000
+      assert account.name == "some name"
     end
 
-    test "update_account/2 with invalid data returns error changeset" do
+    test "update/2 with invalid data returns error changeset" do
       account = account_fixture()
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_account(account, @invalid_attrs)
-      assert account == Accounts.get_account!(account.id)
+      assert {:error, %Ecto.Changeset{}} = Accounts.update(account, @invalid_attrs)
+      assert account == Accounts.get!(account.id)
     end
 
-    test "delete_account/1 deletes the account" do
+    test "delete/1 deletes the account" do
       account = account_fixture()
-      assert {:ok, %Account{}} = Accounts.delete_account(account)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_account!(account.id) end
+      assert {:ok, %Account{}} = Accounts.delete(account)
+      assert_raise Ecto.NoResultsError, fn -> Accounts.get!(account.id) end
     end
 
-    test "change_account/1 returns a account changeset" do
+    test "change/1 returns a account changeset" do
       account = account_fixture()
-      assert %Ecto.Changeset{} = Accounts.change_account(account)
+      assert %Ecto.Changeset{} = Accounts.change(account)
     end
   end
 end
